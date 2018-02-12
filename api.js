9c92ad6749909d9bbd0c8b067390e942455d8050
Kamylle <kamylle.b.g@gmail.com>
2018-02-12 14:03:06 -0500

    Cors installed

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
-const alibay = require('./alibay')
-const express = require('express')
+const alibay = require('./alibay');
+const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 
